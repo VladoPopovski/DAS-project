44f6570c64dc6b87dd9e5a425d12959379f29d07
Marija-Milenkoska <marija.milenkoska15@gmail.com>
2025-01-19 22:45:19 +0100

    Update README.mk

--- Part 4 final/README.mk
+++ Part 4 final/README.mk
@@ -1,8 +1,15 @@
-This folder contains the codes for the microservices, the dockerfiles for all microservises and the docker-compose. 
+This folder contains the codes for the microservices, the dockerfiles for all microservises and the docker-compose it also contai. 
 
 Since the port 5000 for main_service is mapped to 5000,if the Docker containers are running successfully, we should be able to access it via:
 http://localhost:5000
 
 Since the port 8050 for visualization_service is mapped to 8050, if the Docker containers are running successfully, we should be able to access it via:
 http://localhost:8050
+
+This project also includes Azure integration for deploying services to the cloud. The code is designed to be deployed on Microsoft Azure, allowing seamless scalability and cloud accessibility.
+
+
+Deployment:
+Open to see main_service: https://main-service.gentledune-429612a1.westeurope.azurecontainerapps.io
+Open to see visualization_service: https://visualization-service.gentledune-429612a1.westeurope.azurecontainerapps.io
 
