0cd63d9de0062204ada058fbcc7f1f7798fce9e1
Marija-Milenkoska <marija.milenkoska15@gmail.com>
2025-01-19 23:14:18 +0100

    Update README.mk

--- Part 4 final/README.mk
+++ Part 4 final/README.mk
@@ -1,4 +1,4 @@
-This folder contains the codes for the microservices, the dockerfiles for all microservises and the docker-compose it also contai. 
+This folder contains the codes for the microservices, the dockerfiles for all microservises and the docker-compose.
 
 Since the port 5000 for main_service is mapped to 5000,if the Docker containers are running successfully, we should be able to access it via:
 http://localhost:5000
