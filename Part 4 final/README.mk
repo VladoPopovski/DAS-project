e63c03997decba01ec065d1cd0e2951c481a124c
Marija-Milenkoska <marija.milenkoska15@gmail.com>
2025-01-19 22:34:14 +0100

    Create README.mk

---
+++ Part 4 final/README.mk
@@ -0,0 +1,8 @@
+This folder contains the codes for the microservices, the dockerfiles for all microservises and the docker-compose. 
+
+Since the port 5000 for main_service is mapped to 5000,if the Docker containers are running successfully, we should be able to access it via:
+http://localhost:5000
+
+Since the port 8050 for visualization_service is mapped to 8050, if the Docker containers are running successfully, we should be able to access it via:
+http://localhost:8050
+
